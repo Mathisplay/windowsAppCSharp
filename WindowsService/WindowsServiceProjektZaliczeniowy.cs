7bcaf4c7ff17795dbeecb2ad135f5b22ef96cb05
Mateusz Kamiński <mathisplay@gmail.com>
2022-01-24 18:39:26 +0100

    first commit

---
+++ WindowsService/WindowsServiceProjektZaliczeniowy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Thread t;
+        private long memNow;
+        EventLog log;
+        DriveInfo drive;
+        public Service1()
+        {
+            memNow = 0;
+            log = new EventLog("MemProjLog");
+            log.Source = "Service";
+            InitializeComponent();
+        }
+
+        void CheckMem()
+        {
+            drive = new DriveInfo("C");
+            if (drive.IsReady)
+            {
+                memNow = drive.AvailableFreeSpace;
+            }
+            else
+            {
+                memNow = -1;
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            t = new Thread(new ThreadStart(CheckMem));
+            t.Start();
+        }
+
+        protected override void OnStop()
+        {
+            t.Join();
+            log.WriteEntry(memNow.ToString(), EventLogEntryType.Information);
+        }
+    }
+}
---
+++ ZaliczenieLib/MeasurementTool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace ZaliczenieLib
+{
+    public class MeasurementTool
+    {
+        private long mem;
+        public MeasurementTool()
+        {
+            mem = 0;
+        }
+        public long GetMem()
+        {
+            ServiceController service = new ServiceController("ProjektZaliczeniowy");
+            if (service.Status.Equals(ServiceControllerStatus.Stopped) || service.Status.Equals(ServiceControllerStatus.StopPending))
+            {
+                service.Start();
+            }
+            EventLog log = new EventLog();
+            log.Log = "MemProjLog";
+            EventLogEntryCollection e;
+            while(true)
+            {
+                e = log.Entries;
+                if(e.Count != 0)
+                {
+                    mem = Convert.ToInt64(e[e.Count - 1].Message);
+                    log.Source = "Service";
+                    log.Clear();
+                    return mem;
+                }
+            }
+        }
+    }
+}
